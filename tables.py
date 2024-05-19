c83bc19e5af056e1f159dd26517d8600fdaea260
Otmane El Arch <117382024+OTsu01@users.noreply.github.com>
2024-05-19 20:47:40 +0200

    Add files via upload

--- tables.py
+++ tables.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import json
 
+def add_price_per_area(data):
+    for item in data:
+        price = item['Place-Info']['Price']
+        area = item['Place-Info']['Area of Place']
+        price_per_area = price / area if area != 0 else 0
+        item['Place-Info']['Price/Area'] = price_per_area
+    return data
 
 def create_table_from_json(json_file):
     # Read data from JSON file
     with open(json_file, 'r') as f:
         data = json.load(f)
+
+    # Add Price/Area to each dictionary
+    data = add_price_per_area(data)
 
     # Create DataFrame from the JSON data
     df = pd.DataFrame([{
@@ -13,7 +23,10 @@ def create_table_from_json(json_file):
         'Number of Rooms': item['Place-Info']['Number of Rooms'],
         'Area of Place': item['Place-Info']['Area of Place'],
         'Montly Spending': item['Place-Info']['Montly Spending'],
-        'Price': item['Place-Info']['Price']
+        'Price': item['Place-Info']['Price'],
+        'Price/Area': item['Place-Info']['Price/Area']
     } for item in data])
 
+    # Save DataFrame as an HTML file
     return df.to_html('data.html')
+
