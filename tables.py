45f6219f0b32acaaa7a5b4aad3dcbae0f99014a1
Otmane El Arch <117382024+OTsu01@users.noreply.github.com>
2024-05-19 22:47:38 +0200

    Add files via upload

--- tables.py
+++ tables.py
@@ -6,7 +6,7 @@ def add_price_per_area(data):
         price = item['Place-Info']['Price']
         area = item['Place-Info']['Area of Place']
         price_per_area = price / area if area != 0 else 0
-        item['Place-Info']['Price/Area'] = price_per_area
+        item['Place-Info']['Price/Area'] = round(price_per_area, 2)
     return data
 
 def add_price_per_room(data):
@@ -14,7 +14,7 @@ def add_price_per_room(data):
         price = item['Place-Info']['Price']
         rooms = item['Place-Info']['Number of Rooms']
         price_per_room = price / rooms if rooms != 0 else 0
-        item['Place-Info']['Price/Room'] = price_per_room
+        item['Place-Info']['Price/Room'] = round(price_per_room, 2)
     return data
 
 def add_total_cost(data):
@@ -22,18 +22,33 @@ def add_total_cost(data):
         price = item['Place-Info']['Price']
         monthly_spending = item['Place-Info']['Monthly Spending']
         total_cost = price + monthly_spending
-        item['Place-Info']['Total Cost'] = total_cost
+        item['Place-Info']['Total Cost'] = round(total_cost, 2)
+    return data
+def add_yearly_cost(data):
+    for item in data:
+        total_cost = item['Place-Info']['Total Cost']
+        yearly_cost = total_cost * 12
+        item['Place-Info']['Yearly Cost'] = round(yearly_cost, 2)
     return data
 
+def add_yearly_cost_per_area(data):
+    for item in data:
+        yearly_cost = item['Place-Info']['Yearly Cost']
+        area = item['Place-Info']['Area of Place']
+        yearly_price_per_area = yearly_cost / area if area != 0 else 0
+        item['Place-Info']['Yearly Price/Area'] = round(yearly_price_per_area, 2)
+    return data
 def create_table_from_json(json_file):
     # Read data from JSON file
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    # Add Price/Area, Price/Room, and Total Cost to each dictionary
+    # Add Price/Area, Price/Room, Total Cost, yearly cot to each dictionary
     data = add_price_per_area(data)
     data = add_price_per_room(data)
     data = add_total_cost(data)
+    data = add_yearly_cost(data)
+    data = add_yearly_cost_per_area(data)
 
     # Create DataFrame from the JSON data
     df = pd.DataFrame([{
@@ -45,6 +60,7 @@ def create_table_from_json(json_file):
         'Price/Area': item['Place-Info']['Price/Area'],
         'Price/Room': item['Place-Info']['Price/Room'],
         'Total Cost': item['Place-Info']['Total Cost']
+        'Yearly Cost': item['Place-Info']['Yearly Cost']
     } for item in data])
 
     # Save DataFrame as an HTML file
