2efcd9e16561b7dcb6e99a59ea65769df806bc27
Otmane El Arch <117382024+OTsu01@users.noreply.github.com>
2024-05-19 22:12:17 +0200

    Add files via upload

--- tables.py
+++ tables.py
@@ -9,24 +9,43 @@ def add_price_per_area(data):
         item['Place-Info']['Price/Area'] = price_per_area
     return data
 
+def add_price_per_room(data):
+    for item in data:
+        price = item['Place-Info']['Price']
+        rooms = item['Place-Info']['Number of Rooms']
+        price_per_room = price / rooms if rooms != 0 else 0
+        item['Place-Info']['Price/Room'] = price_per_room
+    return data
+
+def add_total_cost(data):
+    for item in data:
+        price = item['Place-Info']['Price']
+        monthly_spending = item['Place-Info']['Monthly Spending']
+        total_cost = price + monthly_spending
+        item['Place-Info']['Total Cost'] = total_cost
+    return data
+
 def create_table_from_json(json_file):
     # Read data from JSON file
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    # Add Price/Area to each dictionary
+    # Add Price/Area, Price/Room, and Total Cost to each dictionary
     data = add_price_per_area(data)
+    data = add_price_per_room(data)
+    data = add_total_cost(data)
 
     # Create DataFrame from the JSON data
     df = pd.DataFrame([{
         'Post Name': item['Place-Info']['Post Name'],
         'Number of Rooms': item['Place-Info']['Number of Rooms'],
         'Area of Place': item['Place-Info']['Area of Place'],
-        'Montly Spending': item['Place-Info']['Montly Spending'],
+        'Monthly Spending': item['Place-Info']['Monthly Spending'],
         'Price': item['Place-Info']['Price'],
-        'Price/Area': item['Place-Info']['Price/Area']
+        'Price/Area': item['Place-Info']['Price/Area'],
+        'Price/Room': item['Place-Info']['Price/Room'],
+        'Total Cost': item['Place-Info']['Total Cost']
     } for item in data])
 
     # Save DataFrame as an HTML file
     return df.to_html('data.html')
-
